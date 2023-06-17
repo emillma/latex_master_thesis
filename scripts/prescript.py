863a87f7fcd7c82382c73f1ab9cecc39aaface34
Emil Martens <emil.martens@gmail.com>
2023-06-17 21:20:48 +0000

    half

--- scripts/prescript.py
+++ scripts/prescript.py
@@ -2,8 +2,8 @@ import argparse
 from pathlib import Path
 
 
-parser = argparse.ArgumentParser(description='Stuff')
-parser.add_argument('--text')
+parser = argparse.ArgumentParser(description="Stuff")
+parser.add_argument("--text")
 args = parser.parse_args()
 print(args)
 # Path.cwd().joinpath('hello.txt').write_text('hello')
