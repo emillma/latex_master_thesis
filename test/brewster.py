facbfdced0774f1012ad8d3bc77432841453d112
Emil Martens <emil.martens@gmail.com>
2023-06-21 15:53:10 +0000

    brewster plot

--- test/brewster.py
+++ test/brewster.py
@@ -6,9 +6,6 @@ import plotly.express as px
 import matplotlib.pyplot as plt
 from pathlib import Path
 
-p_perp = sp.symbols("r_perp", real=True, positive=True)
-p_para = sp.symbols("r_para", real=True, positive=True)
-
 
 n_air = sp.symbols("\\eta_a", real=True, positive=True)
 n_water = sp.symbols("\\eta_w", real=True, positive=True)
@@ -17,15 +14,15 @@ a_air = sp.symbols("\\theta_i", real=True, positive=True)
 a_water = sp.symbols("\\theta_r", real=True, positive=True)
 
 
+a_water_f = sp.asin(sp.sin(a_air) * n_air / n_water)
+# n_water_f = (n_air * sp.sin(a_air)) / sp.sin(a_water)
+
 vals = {
     n_air: 1,
     n_water: 1.33,
+    # n_water: 1.33,
+    a_water: a_water_f,
 }
-
-
-a_water_f = sp.asin(sp.sin(a_air) * n_air / n_water)
-n_water_f = (n_air * sp.sin(a_air)) / sp.sin(a_water)
-
 r_s = (n_air * sp.cos(a_air) - n_water * sp.cos(a_water)) / (
     n_air * sp.cos(a_air) + n_water * sp.cos(a_water)
 )
@@ -34,20 +31,30 @@ r_p = (n_water * sp.cos(a_air) - n_air * sp.cos(a_water)) / (
 )
 
 
-R_s = sp.simplify((r_s).subs(a_water, a_water_f))
-R_p = sp.simplify((r_p).subs(a_water, a_water_f))
+r_s = sp.simplify((r_s).subs(vals))
+r_p = sp.simplify((r_p).subs(vals))
 
 
-print(sp.latex(sp.simplify(r_s)), "\n")
-print(sp.latex(sp.simplify(r_p)), "\n")
-print(sp.latex(R_s), "\n")
-print(sp.latex(R_p), "\n")
+# print(sp.latex(sp.simplify(r_s)), "\n")
+# print(sp.latex(sp.simplify(r_p)), "\n")
+# print(sp.latex(r_s), "\n")
+# print(sp.latex(r_p), "\n")
 # dolp = r_p / r_s
 
-sp.asin(n_air / n_water)
 
-x = np.linspace(0, np.pi / 2, 20)
-f_x = sp.lambdify(a_air, a_water_f.subs(vals), "numpy")
-y = f_x(x)
-fig = plt.plot(x, y)
+x = np.linspace(0, 100, 300)
+a = np.arctan(x)
+f_s = sp.lambdify(a_air, r_s.subs(vals), "numpy")
+f_p = sp.lambdify(a_air, r_p.subs(vals), "numpy")
+fig, ax = plt.subplots()
+
+p = f_p(a)
+s = f_s(a)
+
+
+# ax.plot(x, s**2, label="$s_1$")
+# ax.plot(x, p**2, c="#ff00ff", label="p")
+ax.legend()
+ax.plot(x, np.sqrt(s**4 - p**4) / (s**2 + p**2), label="p")
+# ax.plot(x, s**2 / (s**2 + p**2), label="p")
 plt.savefig(Path(__file__).parent / "brewster.pdf")
